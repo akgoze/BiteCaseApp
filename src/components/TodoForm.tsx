aabbd8e954ba8152d5ef6615a37d941e04302b5e
Fatih Akgöze <fatih@akgoze.dev>
2024-03-03 14:44:57 +0300

    feat(config): Added todo form and methods.

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import TodoItem from "./../models/ToDo";
+import { v4 as uuidv4 } from 'uuid';
+uuidv4();
+
 interface TodoFormProps {
   addTodo: (todoItem: TodoItem) => void;
 }
@@ -7,7 +10,39 @@ interface TodoFormProps {
 
 const TodoForm: React.FC<TodoFormProps> = ({ addTodo }) => {
   const [text, setText] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (text.trim()) {
+      setText('');
+
+      const todoItem: TodoItem = {
+        id: uuidv4(),
+        task: text,
+        completed: false,
+        createdAt: new Date(),
+        isEditing: false
+      };
+
+      addTodo(todoItem);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
   return (
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={text}
+        onChange={handleChange}
+        className='todo-input'
+        placeholder="Enter a todo"
+      />
+      <button className='todo-btn' type="submit">Add Todo</button>
+    </form>
   );
 };
 
