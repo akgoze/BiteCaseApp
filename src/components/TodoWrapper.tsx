5ceb20a8f798fd095034318fa01768e17d55f0a4
Fatih Akgöze <fatih@akgoze.dev>
2024-03-03 14:45:40 +0300

    feat(config): Added edit delete and update methods.

--- src/components/TodoWrapper.tsx
+++ src/components/TodoWrapper.tsx
@@ -14,10 +14,55 @@ export const TodoWrapper: React.FC = () => {
     setTodos([...todos, todoItem])
   }
 
+  const handleToggle = (id: string) => {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    }))
+  }
+
+  const handleEdit = (task:string, id: string) => {
+    setTodos(todos.map(todo => todo.id === id ? {...todo, task, isEditing: false} : todo))
+  }
+
+  const toggleEdit = (id: string) => {
+    setTodos(todos.map(todo => todo.id === id ? {...todo, isEditing: !todo.isEditing} : todo))
+  }
+
+  const handleDelete = (id: string) => {
+    setTodos(
+      todos.map(
+        todo => todo?.id === id 
+          ? {...todo, isEditing: false} 
+          : todo).filter(todo => todo?.id
+                      ? todo.id !== id
+                      : false
+      )
+    )
+    
+  }
+
   return (
     <div className='TodoWrapper'>
       <h1>Todo App</h1>
       
       <TodoForm addTodo={addTodo} />
       
+        {todos.map((todo: TodoItem, index:number) => (
+          todo?.isEditing ?
+          <EditTodoForm
+          todo={todo}
+          editTodo={handleEdit}
+          /> :
+          <Todo
+            key={`task_item_${index}`} 
+            task={todo} 
+            editTask={toggleEdit}
+            toggleTask={handleToggle}
+            deleteTask={handleDelete} />
+        ))}
+    </div>
+  )
 }
